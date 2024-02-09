f042f9ccc1b95ec5407e0eabdd563317782d1b1e
steve whitehouse <stewhites@blueyonder.co.uk>
2024-02-09 00:53:00 +0000

    add reset event function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ let roundEnded = false
 //let clickedPlayButton = document.getElementById('');
 let messagePlayer = document.getElementById("messageplayer");
 let playerChoiceId = ''
-let result ='';
+let result = '';
 let delay = 2000
 const throttledResetGame = throttle(resetGame, delay);
 let playerChoiceIds = [
@@ -22,11 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {});
 //get players choice from DOM (main loop)
 function handleButtonClick() {
   document.getElementById('play').addEventListener("click", function () {
-    console.log("Start button clicked")});
-    document.getElementById('reset').addEventListener("click", function () {
-      console.log("Reset button clicked")});
+    console.log("Start button clicked")
+  });
+  document.getElementById('reset').addEventListener("click", function () {
+    location.reload()
+  });
   playerChoiceIds.forEach(function (playerChoice) {
-  playerChoiceId = document.getElementById(playerChoice);
+    playerChoiceId = document.getElementById(playerChoice);
     if (playerChoiceId) {
       playerChoiceId.addEventListener("click", function () {
         if (!roundEnded) {
@@ -50,45 +52,51 @@ function getComputerTurn(roundEnded) {
 //get result comparison
 function getResult(roundEnded) {
 
-if (playerTurn === computerTurn) {
+  if (playerTurn === computerTurn) {
     messagePlayer.innerText = "It's a draw";
-    result= "draw"
-  } else if ((playerTurn === 'rock' && (computerTurn === 'scissors' || computerTurn === 'lizard')) || (playerTurn === 'paper' && (computerTurn === 'rock' || computerTurn === 'spock')) ||
+    result = "draw"
+  } else if ((playerTurn === 'rock' && (computerTurn === 'scissors' || computerTurn === 'lizard')) ||
+    (playerTurn === 'paper' && (computerTurn === 'rock' || computerTurn === 'spock')) ||
     (playerTurn === 'scissors' && (computerTurn === 'paper' || computerTurn === 'lizard')) ||
     (playerTurn === 'lizard' && (computerTurn === 'spock' || computerTurn === 'paper')) ||
     (playerTurn === 'spock' && (computerTurn === 'scissors' || computerTurn === 'rock'))) {
     messagePlayer.innerText = `You Win, ${playerTurn} beats ${computerTurn}`;
-    result ="win"
+    result = "win"
   } else if (playerTurn == false || computerTurn == false) {
     messagePlayer.innerText = "Make your Choice below"
   } else {
     messagePlayer.innerText = `You Lose, ${computerTurn} beats ${playerTurn}`;
     result = "loss"
   }
-  
+
   return result
 }
 //reset round
 function resetGame() {
-console.log(result);
+  console.log(result);
   playerChoice = null;
   computerChoice = null;
-  if (result =="win") {scores[0] +=1
-  } else if (result =="draw") {scores[1] +=1}
-  else if (result=="loss") {scores[2] +=1 }
+  if (result == "win") {
+    scores[0] += 1
+  } else if (result == "draw") {
+    scores[1] += 1
+  } else if (result == "loss") {
+    scores[2] += 1
+  }
   result = '';
-  document.getElementById('score-won').innerText =scores[0];
-  document.getElementById('score-drew').innerText =scores[1];
-  document.getElementById('score-lost').innerText =scores[2];
+  document.getElementById('score-won').innerText = scores[0];
+  document.getElementById('score-drew').innerText = scores[1];
+  document.getElementById('score-lost').innerText = scores[2];
   roundEnded = false;
   throttle();
 }
+
 function throttle(cb, delay) {
   let wait = false;
 
   return (...args) => {
     if (wait) {
-        return;
+      return;
     }
 
     cb(...args);
